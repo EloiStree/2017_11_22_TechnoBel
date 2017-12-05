bbfe892f0e80abae10bb3615e55c016c2034908d
Unknown <veronique.maboge@gmail.com>
2017-12-05 13:19:11 +0100

    importation Olive Tree

--- UnityProjects/TechnoBelVR/Assets/_Project/Team/Veronique/Rotation.cs
+++ UnityProjects/TechnoBelVR/Assets/_Project/Team/Veronique/Rotation.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    GameObject object1;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetButtonDown())
+        {
+            Instantiate(b, Camera.main.transform.position, Camera.main.transform.rotation);// on tire les balles par rapport à l'endroit où nous sommes.
+
+        }
+
+
+    }
 }
